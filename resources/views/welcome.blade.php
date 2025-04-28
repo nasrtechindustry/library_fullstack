b2d08c9868bfa1fea03795205f82ef2f7ff5fddd
Nasrboy <nasrkihagila@gmail.com>
2025-04-28 03:15:55 +0300

    any

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,10 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
     <!-- Fonts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+    <link rel="stylesheet" href="{{ asset('public/build/assets/app-CYmrHtAS.css') }}">
+    <script type="module" src="{{ asset('public/build/assets/app-CfrlGjIS.js') }}"></script>
+
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
